97f3e2d90abc9ac4e9efe18bf5cecb68d097ccc3
gallopMark <xiaoma920406@126.com>
2017-12-06 17:48:01 +0800

    Failed commit: Default (4)

--- app/src/main/java/com/haoyu/app/entity/EvaluateItemResult.java
+++ app/src/main/java/com/haoyu/app/entity/EvaluateItemResult.java
@@ -2,7 +2,6 @@ package com.haoyu.app.entity;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import com.haoyu.app.adapter.TeachStudyEvaluateAdapter;
 
 import java.util.List;
 
